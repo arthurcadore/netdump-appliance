a792f10beb5d485077966bea381ef8c6a5fe18cd
Arthur Cadore <74879952+arthurcadore@users.noreply.github.com>
2024-06-18 09:36:27 -0300

    :sparkles: added changes to nfdump

--- nfdump/startup.sh
+++ nfdump/startup.sh
@@ -1,22 +1,24 @@
-#!/bin/bash
+#!/bin/ash
 
-NETFLOW_DIR="/var/log/netflow"
-CSV_DIR="/var/log/netflow/csv"
+NETFLOW_DIR="/data/nfdump/raw"
+CSV_DIR="/data/nfdump/csv"
 NETFLOW_PORT=2005
 
 mkdir -p $NETFLOW_DIR
 mkdir -p $CSV_DIR
 
-nfcapd -w -D -p $NETFLOW_PORT -l $NETFLOW_DIR
+nfcapd -T all -l $NETFLOW_DIR -p $NETFLOW_PORT &
 
 convert_to_csv() {
     for file in $NETFLOW_DIR/nfcapd.*; do
-        if [[ -f $file ]]; then
+        if [ -f "$file" ]; then
             timestamp=$(basename $file | cut -d'.' -f2-)
             csv_file="$CSV_DIR/netflow_$timestamp.csv"
-            if [[ ! -f $csv_file ]]; then
-                nfdump -r $file -o csv > $csv_file
+            if [ ! -f "$csv_file" ]; then
+                nfdump -r "$file" -o csv > "$csv_file"
                 echo "Converted $file to $csv_file"
+                rm -f "$file"
+                echo "Deleted original NetFlow file $file"
             fi
         fi
     done
