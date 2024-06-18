ef32f894c8b8531be71c13f9386831fa7f6164af
Arthur Cadore <74879952+arthurcadore@users.noreply.github.com>
2024-06-18 20:52:58 -0300

    :sparkles: added configuration for wainting database

--- nfexporter/startup.sh
+++ nfexporter/startup.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+DB_HOST="database"
+DB_USER="pythonConnector"
+DB_PASSWORD="Python@connect123"
+
+while ! mysqladmin ping -h"$DB_HOST" -u"$DB_USER" -p"$DB_PASSWORD" --silent; do
+    echo "Aguardando o banco de dados estar pronto..."
+    sleep 1
+done
+
+echo "O banco de dados está pronto para aceitar consultas."
+
 python3 /data/nf2csv.py & 
 
 python3 /data/csv2db.py & 
